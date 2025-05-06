6b37171baedc5501c541dca8cf5481419a6c4f92
shraddhamjain7885 <shraddhamjain7885@gmail.com>
2025-05-06 19:21:29 +0530

    Added unit testcase for repository

---
+++ app/src/test/java/com/app/lgb/data/repository/AnimeRepositoryImplTest.kt
@@ -0,0 +1,71 @@
+package com.app.lgb.data.repository
+
+import com.app.core.commondata.Resource
+import com.app.lgb.data.remote.AnimeApiService
+import com.app.lgb.data.remote.AnimeDto
+import com.app.lgb.data.remote.AnimeResponseDto
+import com.app.lgb.data.remote.ImagesDto
+import com.app.lgb.data.remote.JpgDto
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@ExperimentalCoroutinesApi
+class AnimeRepositoryImplTest {
+
+    private lateinit var repository: AnimeRepositoryImpl
+    private val apiService = mockk<AnimeApiService>()
+
+    @Before
+    fun setup() {
+        repository = AnimeRepositoryImpl(apiService)
+    }
+
+    @Test
+    fun `getAnimeList emits loading and success when api call is successful`() = runTest {
+        // Arrange
+        val apiResponse = AnimeResponseDto(
+            data = listOf(
+                AnimeDto(
+                    mal_id = 1,
+                    title = "Naruto",
+                    images = ImagesDto(
+                        jpg = JpgDto(image_url = "image_url")
+                    ),
+                    synopsis = "A ninja story"
+                )
+            )
+        )
+        coEvery { apiService.getAnimeList() } returns apiResponse
+
+        // Act
+        val emissions = repository.getAnimeList().toList()
+
+        // Assert
+        assert(emissions[0] is Resource.Loading)
+        assert(emissions[1] is Resource.Success)
+        val items = (emissions[1] as Resource.Success).data
+        assertEquals("Naruto", items!![0].title)
+        assertEquals("image_url", items[0].imageUrl)
+    }
+
+    @Test
+    fun `getAnimeList emits loading and error when api call throws exception`() = runTest {
+        // Arrange
+        coEvery { apiService.getAnimeList() } throws RuntimeException("API failure")
+
+        // Act
+        val emissions = repository.getAnimeList().toList()
+
+        // Assert
+        assert(emissions[0] is Resource.Loading)
+        assert(emissions[1] is Resource.Error)
+        val errorMessage = (emissions[1] as Resource.Error).message
+        assertEquals("API failure", errorMessage)
+    }
+}
